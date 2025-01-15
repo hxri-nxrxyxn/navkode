8b4de240b86c6a461f06ebd40f2a584ce31edb7b
Sebastian <sebastianabraham2006@gmail.com>
2025-01-15 16:52:52 +0530

    implmented database connecection

--- api/main.go
+++ api/main.go
@@ -1,12 +1,25 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/navkode/database"
+)
 
 func main() {
+
+	db, err := database.NewConnection()
+	if err != nil {
+		log.Fatal("Could not load database")
+	}
+	
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World")
 	})
+
+	
 
 	app.Listen(":8080")
 }
