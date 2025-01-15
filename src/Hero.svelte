1f413af7243cb63b910bb3c332436062d32ca1f0
Hari Narayan <harinarayanmr@aol.com>
2025-01-15 18:43:54 +0530

    just done first page

---
+++ src/Hero.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { Link } from "svelte-routing";
+    const categories = ["Malls", "Monuments"];
+    let category = $state("");
+
+    let i = 0;
+    category = categories[i];
+
+    const goLeft = () => {
+        if (i != 0) {
+            console.log(i);
+            category = categories[--i];
+        }
+    };
+    const goRight = () => {
+        if (i < categories.length - 1) {
+            console.log(i);
+            category = categories[++i];
+        }
+    };
+</script>
+
+<main>
+    <h1>Choose your destination</h1>
+
+    <div class="logo__box"></div>
+
+    <div class="option__box">
+        <div onclick={goLeft}>left</div>
+        <div class="option__text">
+            <h2>{category}</h2>
+        </div>
+        <div onclick={goRight}>right</div>
+    </div>
+</main>
+
+<style>
+</style>
